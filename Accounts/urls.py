488a83f371be69a73dbbd10ddacf07bcaee645bc
Will Wagner <willwagner602@gmail.com>
2016-09-19 12:08:57 -0400

    - Fixed bugs in user accounts where duplicate accounts weren't dealt with properly - Fixed issues where account views didn't resolve correctly

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url, include
-from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     url(r'^create_user$', views.create_user, name="create_user"),
+    url(r'^login$', views.login_user, name='login'),
     url(r'^', include('django.contrib.auth.urls'))
 ]
--- Accounts/views.py
+++ Accounts/views.py
@@ -1,27 +1,49 @@
 __author__ = 'wbw'
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 
 def create_user(request):
     if request.method == 'POST':
         user_info = request.POST
 
-        # ToDo: Figure out why this errors the the first time a user logs in and never again
-        username = user_info['email']
+        username = user_info['username']
         password = user_info['password']
         password_confirmation = user_info['password_confirmation']
-
-        print(user_info)
+        email = user_info['email']
 
         if password != password_confirmation:
             # return error
             return render(request, 'registration/create_user.html', {'error': 'passwords do not match'})
+        try:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            print(user)
+        except IntegrityError as e:
+            print(request.__dict__)
+            return render(request, 'registration/create_user.html', {'error': 'This username is not valid'})
 
-        user = User.objects.create_user(username, email=username, password=password)
-
-        return render(request, 'registration/login.html', {'success': 'User created successfully!'})
+        return redirect('/user/login', success = 'User created successfully!', username = user.username)
 
     else:
         return render(request, 'registration/create_user.html', {})
+
+
+def login_user(request):
+    if request.user.is_authenticated:
+        redirect('/')
+    elif request.method == 'POST':
+        print(request.POST)
+    else:
+        print('test')
+        return render(request, 'registration/login.html', {'username': 'username', 'shit':'shit'})
+
+
+def logout(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return render(request, 'registration/logout.html', {})
+    else:
+        return render(request, '/')
